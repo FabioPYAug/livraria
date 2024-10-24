5ddfbe422bde58ae5e1b0ae2d295795350da5724
FabioPYAug <fabioacv2403@gmail.com>
2024-10-24 11:22:29 -0300

    FINALIZADO O PASSO 7

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,12 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet
+from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet
 
 router = DefaultRouter()
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
+router.register(r"autores", AutorViewSet)
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 
 urlpatterns = [
