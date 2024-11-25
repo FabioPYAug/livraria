c8a1c00d362a1bf60a89ffae2d9b27054e0b4aa5
FabioPYAug <fabioacv2403@gmail.com>
2024-11-25 09:44:08 -0300

    PASSO 12 FEITO

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -9,6 +11,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from uploader.router import router as uploader_router
 from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
@@ -32,9 +35,13 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    path("api/media/", include(uploader_router.urls)),
     # Simple JWT
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # API
     path("api/", include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
+
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,10 +1,21 @@
-from rest_framework.serializers import ModelSerializer
 from core.models import Livro
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 class LivroSerializer(ModelSerializer):
-    class Meta:
-        model = Livro
-        fields = "__all__"
+    capa_attachment_key = SlugRelatedField(
+        source="capa",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(
+        required=False,
+        read_only=True
+    )
 
 class LivroListSerializer(ModelSerializer):
     class Meta:
@@ -16,3 +27,4 @@ class LivroRetrieveSerializer(ModelSerializer):
         model = Livro
         fields = "__all__"
         depth = 1
+        capa = ImageSerializer(required=False)
