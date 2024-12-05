218c75fbc8159d564feb0ef1c449d24db9ec98ae
FabioPYAug <fabioacv2403@gmail.com>
2024-12-05 08:07:11 -0300

    PASSO 27 FEITO :D

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -10,8 +10,13 @@ class ItensCompraSerializer(ModelSerializer):
 
     class Meta:
         model = ItensCompra
-        fields = ("livro", "quantidade", "total")
+        fields = ("id", "usuario", "status", "total", "itens")
         depth = 1
+
+class ItensCompraCreateUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
 
 class CompraSerializer(ModelSerializer):
     status = CharField(source="get_status_display", read_only=True)
@@ -20,3 +25,18 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = "__all__"
+
+class CompraCreateUpdateSerializer(ModelSerializer):
+    itens = ItensCompraCreateUpdateSerializer(many=True)
+
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
