4c4dc0114b4c129d9e29f8af0893db6a0c35802f
FabioPYAug <fabioacv2403@gmail.com>
2024-12-16 09:34:09 -0300

    PASSO 29 FEITO

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,4 +1,10 @@
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import (
+    CharField,
+    CurrentUserDefault, # novo
+    HiddenField, # novo
+    ModelSerializer,
+    SerializerMethodField,
+)
 
 from core.models import Compra, ItensCompra
 
@@ -35,6 +41,7 @@ class CompraSerializer(ModelSerializer):
         fields = "__all__"
 
 class CompraCreateUpdateSerializer(ModelSerializer):
+    usuario = HiddenField(default=CurrentUserDefault())
     itens = ItensCompraCreateUpdateSerializer(many=True)
 
     class Meta:
