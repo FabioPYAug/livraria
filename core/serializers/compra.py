835956bf6d1465e65486060db9e3d8e49ea7f963
FabioPYAug <fabioacv2403@gmail.com>
2024-12-19 07:55:57 -0300

    PASSO 30 FEITO

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,9 +1,11 @@
 from rest_framework.serializers import (
     CharField,
-    CurrentUserDefault, # novo
-    HiddenField, # novo
+    CurrentUserDefault,
+    DateTimeField,
+    HiddenField,
     ModelSerializer,
     SerializerMethodField,
+    ValidationError,
 )
 
 from core.models import Compra, ItensCompra
@@ -16,8 +18,23 @@ class ItensCompraSerializer(ModelSerializer):
 
     class Meta:
         model = ItensCompra
-        fields = ("id", "usuario", "status", "total", "itens")
+        fields = ("livro", "quantidade", "total")
         depth = 1
+
+class ItensCompraCreateUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
+    def validate_quantidade(self, quantidade):
+        if quantidade <= 0:
+            raise ValidationError("A quantidade deve ser maior do que zero.")
+        return quantidade
+
+    def validate(self, item):
+        if item["quantidade"] > item["livro"].quantidade:
+            raise ValidationError("Quantidade de itens maior do que a quantidade em estoque.")
+        return item
 
 class ItensCompraListSerializer(ModelSerializer):
     livro = CharField(source="livro.titulo", read_only=True)
@@ -27,33 +44,42 @@ class ItensCompraListSerializer(ModelSerializer):
         fields = ("quantidade", "livro")
         depth = 1
 
-class ItensCompraCreateUpdateSerializer(ModelSerializer):
-    class Meta:
-        model = ItensCompra
-        fields = ("livro", "quantidade")
-
 class CompraSerializer(ModelSerializer):
-    status = CharField(source="get_status_display", read_only=True)
     usuario = CharField(source="usuario.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
+    data = DateTimeField(read_only=True)
+    tipo_pagamento = CharField(source="get_tipo_pagamento_display", read_only=True) # novo campo
     itens = ItensCompraSerializer(many=True, read_only=True)
+
     class Meta:
         model = Compra
-        fields = "__all__"
+        fields = ("id", "usuario", "status", "total", "data", "tipo_pagamento", "itens")
+
 
 class CompraCreateUpdateSerializer(ModelSerializer):
+    itens = ItensCompraCreateUpdateSerializer(many=True) # Aqui mudou
     usuario = HiddenField(default=CurrentUserDefault())
-    itens = ItensCompraCreateUpdateSerializer(many=True)
-
     class Meta:
         model = Compra
         fields = ("usuario", "itens")
 
+    def create(self, validated_data):
+        itens = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item in itens:
+            item["preco"] = item["livro"].preco # nova linha
+            ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
+        return compra
+    
     def update(self, compra, validated_data):
-        itens_data = validated_data.pop("itens")
-        if itens_data:
+        itens = validated_data.pop("itens")
+        if itens:
             compra.itens.all().delete()
-            for item_data in itens_data:
-                ItensCompra.objects.create(compra=compra, **item_data)
+            for item in itens:
+                item["preco"] = item["livro"].preco  # nova linha
+                ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
         return super().update(compra, validated_data)
 
 class CompraListSerializer(ModelSerializer):
