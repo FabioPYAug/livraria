fb1639e2025130bb07ca83d7f0cfe33b2293b4c0
FabioPYAug <fabioacv2403@gmail.com>
2025-02-03 09:48:30 -0300

    PASSO 36 FEITO

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from django.db.models.aggregates import Sum
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.response import Response
@@ -15,6 +16,8 @@ from core.serializers import (
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["categoria__descricao", "editora__nome"]
 
     def get_serializer_class(self):
         if self.action == "list":
