afda34fe81405c25537f2b9fe494c3f95221b489
FabioPYAug <fabioacv2403@gmail.com>
2025-02-03 09:28:28 -0300

    PASSO 35 FEITO

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,7 +1,16 @@
+from rest_framework import status
+from django.db.models.aggregates import Sum
+from rest_framework.decorators import action
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import Livro
-from core.serializers import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
-
+from core.serializers import (
+    LivroAlterarPrecoSerializer,
+    LivroListSerializer,
+    LivroRetrieveSerializer,
+    LivroSerializer,
+)
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
@@ -13,5 +22,34 @@ class LivroViewSet(ModelViewSet):
         elif self.action == "retrieve":
             return LivroRetrieveSerializer
         return LivroSerializer
+
+    @action(detail=True, methods=["patch"])
+    def alterar_preco(self, request, pk=None):
+        livro = self.get_object()
+
+        serializer = LivroAlterarPrecoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        livro.preco = serializer.validated_data["preco"]
+        livro.save()
+
+        return Response(
+            {"detail": f"Preço do livro '{livro.titulo}' atualizado para {livro.preco}."}, status=status.HTTP_200_OK
+        )
+
+    @action(detail=False, methods=["get"])
+    def mais_vendidos(self, request):
+        livros = Livro.objects.annotate(total_vendidos=Sum("itenscompra__quantidade")).filter(total_vendidos__gt=10)
+
+        data = [
+            {
+                "id": livro.id,
+                "titulo": livro.titulo,
+                "total_vendidos": livro.total_vendidos,
+            }
+            for livro in livros
+        ]
+
+        return Response(data, status=status.HTTP_200_OK)
     
     
